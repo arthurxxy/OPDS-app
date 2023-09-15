6bde05c6634c5a87e82a28cd6b953a16d9f9a3bd
sean <arthurxxy@gmail.com>
2023-09-15 12:22:04 +0800

    Initial Commit

---
+++ OPDS/OPDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OPDSApp.swift
+//  OPDS
+//
+//  Created by sean on 2023/9/15.
+//
+
+import SwiftUI
+
+@main
+struct OPDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
